6cb1acd6f04c8b99341e9b847473cc71216cc454
nomaddevs1 <farouqsiwoku@gmail.com>
2023-11-04 14:30:05 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box textAlign="center" fontSize="xl">
+      <Text>Welcome to React with TypeScript and Chakra UI!</Text>
+    </Box>
+  );
+}
+
+export default App;
+
